dc13e7fa10f218d7e83b1f86344cfbc5185ba98c
McB0y <rafael.cisneros.gomez@gmail.com>
2017-02-06 21:11:43 -0600

    TypeScript
    Una observación sobre los módulos en TypeScript, y herramientas como traceur.

--- main.ts
+++ main.ts
@@ -6,3 +6,7 @@ function sayHi(animal:Animal){
 }
 
 sayHi(new Dog("Timmy"));
+
+//La manera en la que se estan utilizando la importación de módulos 
+//no estan dificil si se implementa typescipt pero haciendo lo con vanillaJS o traceur
+//se me hace una buena elección también.
