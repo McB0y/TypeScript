32117b7f98fc5f74968f8d7cbac19b9f8ef41fc8
McB0y <rafael.cisneros.gomez@gmail.com>
2017-04-18 20:08:10 -0500

    basico

---
+++ basico/let.js
@@ -0,0 +1,10 @@
+var foo = 123;
+if (true) {
+    var foo = 456;
+}
+console.log(foo);
+var foo1 = 123;
+if (true) {
+    var foo1_1 = 456;
+}
+console.log(foo1);
---
+++ basico/tipos.ts
@@ -0,0 +1,11 @@
+let isDone :boolean = false;
+let decimal : number= 6;
+let hex : number = 0xf00d;
+let binary : number = 0b1010;
+let octal : number= 0o744;
+let color : string = "blue";
+color = "red";
+let fullName : string = "Bob Bobington";
+let age : number = 37;
+let sentence : string = `Hola mi nombre es ${ fullName }
+I'll be ${ age + 1 } years old next month`; 
