97cab4506bf149fb79212de748bc4ac55d1417b4
McB0y <rafael.cisneros.gomez@gmail.com>
2017-04-18 20:10:28 -0500

    Eliminando basico

--- basico/saluda.ts
+++
@@ -1,9 +0,0 @@
-function saluda (persona:string){
-   return "hola, " + persona;
-}
-
-var usuario = "Alex Market";
-document.body.innerHTML = saluda(usuario);
-
-// var usuario = [1,2,3];
-// document.body.innerHTML =saluda(usuario);
